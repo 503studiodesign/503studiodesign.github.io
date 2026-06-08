6b664c1bec3b203e69c82b21dda6dd4e09aff1a1
503studiodesign <rrafinha@gmail.com>
2026-06-08 17:18:38 -0300

    script.js

--- script.js
+++ script.js
@@ -16,3 +16,45 @@ elements.forEach(el => {
   el.style.transition = "0.6s";
   observer.observe(el);
 });
+
+// Scrolling IMG ao rolar
+const frameCount = 60; // quantidade de imagens
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+canvas.width = 1280;
+canvas.height = 720;
+
+// gera caminho das imagens
+const currentFrame = index => {
+  return `images/frame_${String(index).padStart(4, '0')}.jpg`;
+};
+
+const images = [];
+const imageObj = new Image();
+
+// pré-carregar imagens
+for (let i = 1; i <= frameCount; i++) {
+  const img = new Image();
+  img.src = currentFrame(i);
+  images.push(img);
+}
+
+// desenha primeira imagem
+images[0].onload = () => {
+  context.drawImage(images[0], 0, 0);
+};
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
+
+  const scrollFraction = scrollTop / maxScroll;
+  const frameIndex = Math.min(
+    frameCount - 1,
+    Math.floor(scrollFraction * frameCount)
+  );
+
+  context.drawImage(images[frameIndex], 0, 0);
+});
